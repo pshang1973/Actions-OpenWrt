be5975cc3643f65b4bec144838b922d583ae3daa
pshang1973 <70361490+pshang1973@users.noreply.github.com>
2020-08-31 16:40:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+got clone https://github.com/V2RaySSR/openwrt-packages
+git clone https://github.com/V2RaySSR/small
+git clone https://github.com/tuanqing/install-program package/install-program
